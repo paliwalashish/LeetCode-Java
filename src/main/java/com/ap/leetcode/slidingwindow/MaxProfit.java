cf161f9cf932b4bb1f2e1881626c3f3d4e1ada32
paliwalashish <paliwalashish@gmail.com>
2024-02-07 17:56:07 -0800

    Added MaxProfit

---
+++ src/main/java/com/ap/leetcode/slidingwindow/MaxProfit.java
@@ -0,0 +1,19 @@
+package com.ap.leetcode.slidingwindow;
+
+public class MaxProfit {
+    public int maxProfit(int[] prices) {
+        int left = 0;
+        int right = 1;
+        int maxProfit = 0;
+        while(right < prices.length) {
+            if(prices[left] < prices[right]) {
+                maxProfit = Math.max(maxProfit, prices[right] - prices[left]);
+                right++;
+            } else {
+                left = right;
+                right++;
+            }
+        }
+        return maxProfit;
+    }
+}
